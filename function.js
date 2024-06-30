dec0babc492c0e6d55da771499f3fe4364863df2
Abdul Moiz Khan <75619175+MoizKhan07@users.noreply.github.com>
2024-06-30 16:49:14 +0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+$(window).scroll(function(){
+  if($(window).scrollTop() > 0) {
+    $('.navbar').css('background','#212529');
+  }
+  else{
+    $('.navbar').css('background','transparent');
+  }
+})
+
+$('.reviews-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items: 1,
+})
